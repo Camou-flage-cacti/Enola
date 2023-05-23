5674233a591c50ed4c3ef7fb78d3d8332631e685
Tomal-kuet <armanuzzamankuetcse@gmail.com>
2023-05-23 17:29:00 -0400

    beq inline asm and capstone test

--- Test_tz/test_ss/instrumented.c
+++ Test_tz/test_ss/instrumented.c
@@ -4,15 +4,17 @@
 int frwd_edge()
 {
 		__asm volatile(
-	  //"LDR r0, [sp, #4]\n\t"
-		//"CMP r0, #5\n\t"
-		//"BEQ 0x11000C9C\n\t"
+	 //"LDR r0, [sp, #4]\n\t"
+		"CMP r0, #5\n\t"
+		//"BEQ 0xff\n\t"
 		"PUSH {r1}\n\t"
-		"MOV r1, #0x0C9C\n\t"
-		//"PACG r12, r1, r12\n\t"
-		//"MOV r1, 0x11000CAC\n\t"
-		//"PACG r12, r1, r12\n\t"
-		//"POP r1\n\t"
+		"MOV r1, #0x0CA4\n\t"
+		"MOVT r1, #0x1100\n\t"
+		"PACG r12, r1, r12\n\t"
+		"MOV r1, 0x0CB4\n\t"
+		"MOVT r1, #0x1100\n\t"
+		"PACG r12, r1, r12\n\t"
+		"POP {r1}\n\t"
 	);
 		return 1;
 }
