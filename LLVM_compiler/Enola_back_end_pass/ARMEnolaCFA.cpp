6336609266355bde49438afd7767a250f564c0d3
Tomal-kuet <armanuzzamankuetcse@gmail.com>
2023-10-11 04:21:22 -0400

    machine instruction builder stdout and pacbit feature hceck

--- LLVM_compiler/Enola_back_end_pass/ARMEnolaCFA.cpp
+++ LLVM_compiler/Enola_back_end_pass/ARMEnolaCFA.cpp
@@ -44,12 +44,47 @@ bool ARMEnolaCFA::instrumentRet (MachineBasicBlock &MBB,
    // if (TII.getIns)
     
     MIB = BuildMI(MBB, MI, DL,TII.get(ARM::tCMPi8)).addReg(ARM::R12).addImm(0).add(predOps(ARMCC::AL)).setMIFlag(MachineInstr::NoFlags);
-    
+
+    MachineInstr *MI3 = MIB;
+
+    std::string instructionString2;
+    llvm::raw_string_ostream OS2(instructionString2);
+    MI3->print(OS2);
+    outs()<<"constructed instruction in string cmp: "<<instructionString2<<"\n";
+
   //  MIB = BuildMI(MBB, MI, DL, TII.get(ARM::t2PAC)).add(predOps(ARMCC::AL)).setMIFlag(MachineInstr::NoFlags);
-    //MIB = BuildMI(MBB, MI, DL, TII.get(ARM::t2PACG)).addReg(ARM::R1).addReg(ARM::R0).addReg(ARM::R2).add(predOps(ARMCC::AL)).setMIFlag(MachineInstr::NoFlags);
+    MIB = BuildMI(MBB, MI, DL, TII.get(ARM::t2PACG)).addReg(ARM::R1).addReg(ARM::R0).addReg(ARM::R2).add(predOps(ARMCC::AL)).setMIFlag(MachineInstr::NoFlags);
 
     outs() << "Consructed instructions: " << MIB <<"\n";
+    MachineInstr *MI2 = MIB;
 
+    // Convert the MachineInstr to a string representation.
+    std::string instructionString;
+    llvm::raw_string_ostream OS(instructionString);
+    MI2->print(OS);
+    outs()<<"constructed instruction in string : "<<instructionString<<"\n";
+
+    /*
+    
+    MachineFunction &MF = ...; // Your MachineFunction
+
+    // Get the MachineBasicBlock where you want to insert the inline assembly code.
+    MachineBasicBlock &MBB = MF.front(); // You can choose a different basic block as needed.
+
+    const TargetRegisterInfo *TRI = MF.getSubtarget().getRegisterInfo();
+
+    // Create an InlineAsm object with your inline assembly code.
+    const char *AssemblyCode = "your assembly code here";
+    const char *ConstraintString = "constraint string here";
+    unsigned NumOperands = 0; // The number of operands your assembly code expects.
+
+    InlineAsm *IA = InlineAsm::get(InlineAsm::KindTy::AD_ATT, AssemblyCode, ConstraintString, NumOperands);
+
+    // Create a MachineInstr with the inline assembly code.
+    MachineInstrBuilder MIB = BuildMI(MBB, MBB.end(), DebugLoc(), TII->get(TargetOpcode));
+
+    // Add the InlineAsm object as an operand.
+    MIB.addExternalSymbol(IA);*/
     return true;
     
 
@@ -59,6 +94,11 @@ bool ARMEnolaCFA::runOnMachineFunction(MachineFunction &MF) {
     bool modified = false;
 
     std::string MFName = MF.getName().str();
+
+    if (MF.getSubtarget().getFeatureBits()[ARM::FeaturePACBTI])
+    {
+        outs() <<"PAC bit feature exists\n";
+    }
     outs() << "Enola Instrumentation: "<<MFName<<"\n";
     const ARMBaseInstrInfo &TII = *static_cast<const ARMBaseInstrInfo *>(MF.getSubtarget().getInstrInfo());
    // const TargetInstrInfo *x =  MF.getSubtarget().getInstrInfo();
@@ -71,7 +111,7 @@ bool ARMEnolaCFA::runOnMachineFunction(MachineFunction &MF) {
         //outs() << "Contents of BasicBlock corresponding to MachineBasicBlock:\n";
         //outs() << BB << "\n";
         
-
+ 
         for(auto &MI:MBB){
 
             if(MI.getDesc().isCompare())
