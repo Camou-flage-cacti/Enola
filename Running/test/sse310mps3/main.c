745952b4ad7981eca2e7ac2b6aa9158419db6d4f
XI TAN <mintancy@hotmail.com>
2024-01-24 13:37:06 -0500

    test makefile

---
+++ Running/test/sse310mps3/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "Driver_USART.h"
+#include "stdout.h"
+
+static FILE __stdio = FDEV_SETUP_STREAM(stdout_putchar, NULL, NULL, _FDEV_SETUP_WRITE);
+FILE *const stdin = &__stdio;
+__strong_reference(stdin, stdout);
+__strong_reference(stdin, stderr);
+
+
+int main(void)
+{
+		stdout_init();
+		printf("\r\n= Hello World =\r\n");
+}
