82e6c3d9b43861bcc17c7fd5cddb41339f20139c
Tomal-kuet <armanuzzamankuetcse@gmail.com>
2024-01-31 19:39:59 -0500

    integrate Enola passes into llvm ARM

--- tool/all-runner.sh
+++ tool/all-runner.sh
@@ -5,6 +5,6 @@ fi
 
 cFile="$1"
 
-clang $cFile -S -emit-llvm -o out.ll
-opt -enable-new-pm=0 -load /home/tomal/llvm_all/llvm-project/build/lib/LLVMEnolaPass.so -EnolaPass out.ll -S -o out_opt.ll > /dev/null
-llc -march=arm -mcpu=cortex-m85 -O0 out_opt.ll -mattr=+pacbti -o outm_pass.s
+/home/tomal/llvm_all/llvm-armv16/LLVM-embedded-toolchain-for-Arm-release-16.0.0/build/llvm/bin/clang $cFile -S -emit-llvm -o out.ll
+/home/tomal/llvm_all/llvm-armv16/LLVM-embedded-toolchain-for-Arm-release-16.0.0/build/llvm/bin/opt -enable-new-pm=0 -load /home/tomal/llvm_all/llvm-armv16/LLVM-embedded-toolchain-for-Arm-release-16.0.0/build/llvm/lib/LLVMEnolaPass.so -EnolaPass out.ll -S -o out_opt.ll > /dev/null
+/home/tomal/llvm_all/llvm-armv16/LLVM-embedded-toolchain-for-Arm-release-16.0.0/build/llvm/bin/llc -march=arm -mcpu=cortex-m85 -O0 out_opt.ll -mattr=+pacbti -o outm_passARM.s
