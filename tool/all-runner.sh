cd550e586180b668b3e45a22691d66030595ec5e
Camou-flage-cacti <dakumiju@gmail.com>
2024-04-29 09:36:44 -0400

    path

--- tool/all-runner.sh
+++ tool/all-runner.sh
@@ -5,6 +5,6 @@ fi
 
 cFile="$1"
 
-/home/tomal/llvm_all/llvm-armv16/LLVM-embedded-toolchain-for-Arm-release-16.0.0/build/llvm/bin/clang $cFile -S -emit-llvm -o out.ll
-/home/tomal/llvm_all/llvm-armv16/LLVM-embedded-toolchain-for-Arm-release-16.0.0/build/llvm/bin/opt -enable-new-pm=0 -load /home/tomal/llvm_all/llvm-armv16/LLVM-embedded-toolchain-for-Arm-release-16.0.0/build/llvm/lib/LLVMEnolaPass.so -EnolaPass out.ll -S -o out_opt.ll > /dev/null
-/home/tomal/llvm_all/llvm-armv16/LLVM-embedded-toolchain-for-Arm-release-16.0.0/build/llvm/bin/llc -march=arm -mcpu=cortex-m85 -O0 out_opt.ll -mattr=+pacbti -o outm_passARM.s
+root_path_of_llvm/LLVM-embedded-toolchain-for-Arm-release-16.0.0/build/llvm/bin/clang $cFile -S -emit-llvm -o out.ll
+root_path_of_llvm/LLVM-embedded-toolchain-for-Arm-release-16.0.0/build/llvm/bin/opt -enable-new-pm=0 -load root_path_of_llvm/LLVM-embedded-toolchain-for-Arm-release-16.0.0/build/llvm/lib/LLVMEnolaPass.so -EnolaPass out.ll -S -o out_opt.ll > /dev/null
+root_path_of_llvm/LLVM-embedded-toolchain-for-Arm-release-16.0.0/build/llvm/bin/llc -march=arm -mcpu=cortex-m85 -O0 out_opt.ll -mattr=+pacbti -o outm_passARM.s
